579f273590b86cee025e6a456031558fe1d078a9
XCezor <xcezor@gmail.com>
2025-01-19 16:08:23 +0100

    Functions fixes

--- main.py
+++ main.py
@@ -28,22 +28,19 @@ class Movie:
 		pattern = r"^(?:[01]\d|2[0-3]):[0-5]\d-(?:[01]\d|2[0-3]):[0-5]\d$"
 		return bool(re.match(pattern, showtime))
 
-	def add_showtime(self, time:list):
-		for showtime in time:
-			if not self.validate_showtime(showtime):
-				raise ValueError("Showtime must be in correct range: 00-00 to 23-59.")
+	def add_showtime(self, time:str):
+		if not self.validate_showtime(time):
+			raise ValueError("Showtime must be in correct range: 00-00 to 23-59.")
 
-			for object in time:
-				if object in self.showtimes:
-					print("One of the requested showtimes already exists for that movie. It will be skipped.")
-					time.remove(object)
+		if time in self.showtimes:
+			print("Requested showtime already exists for that movie. It will be skipped.")
+			return False
 
 		self.showtimes.append(time)
 
-	def remove_showtime(self, time:list):
-		for object in time:
-			if object in self.showtimes:
-				self.showtimes.remove(object)
+	def remove_showtime(self, time:str):
+		if time in self.showtimes:
+			self.showtimes.remove(time)
 		print("All showtimes removed successfully, if there were any.")
 
 	def display_details(self):
@@ -55,4 +52,10 @@ class Movie:
 		print(details)
 
 new_movie = Movie("Titanic", "150", ["12:30-12:50", "14:00-15:50"])
+new_movie.display_details()
+
+new_movie.add_showtime("19:29-22:33")
+new_movie.display_details()
+
+new_movie.remove_showtime("12:30-12:50")
 new_movie.display_details()
